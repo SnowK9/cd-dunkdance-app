877d667b86401cbaa7f86da387ca27873b2c56d9
Jessica <snowk9668@gmail.com>
2026-07-14 14:08:29 -0700

    update app text to include Daisy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jessica's DunkDance App is live! Jessica and Daisy!
         </p>
         <a
           className="App-link"
